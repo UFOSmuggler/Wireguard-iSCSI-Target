46abd1e861c2701635cb176a485e8539dd091b7c
David Zielezna <davidz@brconsulting.com.au>
2024-01-18 04:29:45 +0000

    fix wg script ip

--- root/defaults/wg0.preup.sh
+++ root/defaults/wg0.preup.sh
@@ -10,4 +10,4 @@ iptables -A wg0-input -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A wg0-input -p tcp --dport 3260 -j wg0-input-iscsi
 iptables -A wg0-input -j REJECT
 
-iptables -A wg0-input-iscsi -s 172.16.222.2 -j ACCEPT
+iptables -A wg0-input-iscsi -s 172.16.223.2 -j ACCEPT
